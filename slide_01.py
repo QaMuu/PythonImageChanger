1b61ef62c276410eddb3a7953cc3fdcdaf5069ea
QaMuu <44154881+QaMuu@users.noreply.github.com>
2025-10-06 15:40:55 +0900

    教材1まで終了

---
+++ slide_01.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+# ウィンドウの準備
+root = Tk()
+root.title("ボタン設置")
+root.minsize(400, 200)
+root.maxsize(400, 200)
+
+root.mainloop()
---
+++ tkinter_1.py
@@ -0,0 +1,33 @@
+from tkinter import *
+
+# ウィンドウの準備
+root = Tk()
+root.title("ボタン設置")
+root.minsize(400, 200)
+root.maxsize(400, 200)
+
+# 関数の設置
+def close_window():
+    root.destroy()
+
+def change_message():
+    get_name = entry_message.get()
+    label_message["text"] = "こんにちわ、" + get_name + "！"
+    entry_message.delete(0, END)
+
+# ウィジェットの設置
+label_message = Label(root, text = "Hello!")
+label_message.pack()
+
+entry_message = Entry(root)
+entry_message.pack()
+
+button_message = Button(root, text = "Message", command = change_message)
+button_message.pack()
+
+button_end = Button(root, text = "End", command = close_window)
+button_end.pack()
+
+
+
+root.mainloop()
