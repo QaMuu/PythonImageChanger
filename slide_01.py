065ab75e65d6e051887cf5742424546f0f7202f0
QaMuu <44154881+QaMuu@users.noreply.github.com>
2025-10-06 18:03:08 +0900

    修正Push

--- slide_01.py
+++ slide_01.py
@@ -6,4 +6,24 @@ root.title("ボタン設置")
 root.minsize(400, 200)
 root.maxsize(400, 200)
 
+label_message = Label(root, text = "Hello!")
+label_message.pack()
+
+def change_message():
+    get_name = entry_message.get()
+    label_message["text"] = "こんにちわ、" + get_name + "！"
+    entry_message.delete(0, END)
+
+button_message = Button(root, text = "Message", command = change_message)
+button_message.pack()
+
+entry_message = Entry(root)
+entry_message.pack()
+
+def close_window():
+    root.destroy()
+
+button_end = Button(root, text = "End", command = close_window)
+button_end.pack()
+
 root.mainloop()
--- tkinter_1.py
+++ tkinter_1.py
@@ -6,28 +6,24 @@ root.title("ボタン設置")
 root.minsize(400, 200)
 root.maxsize(400, 200)
 
-# 関数の設置
-def close_window():
-    root.destroy()
+label_message = Label(root, text = "Hello!")
+label_message.pack()
 
 def change_message():
     get_name = entry_message.get()
     label_message["text"] = "こんにちわ、" + get_name + "！"
     entry_message.delete(0, END)
 
-# ウィジェットの設置
-label_message = Label(root, text = "Hello!")
-label_message.pack()
+button_message = Button(root, text = "Message", command = change_message)
+button_message.pack()
 
 entry_message = Entry(root)
 entry_message.pack()
 
-button_message = Button(root, text = "Message", command = change_message)
-button_message.pack()
+def close_window():
+    root.destroy()
 
 button_end = Button(root, text = "End", command = close_window)
 button_end.pack()
-
-
 
 root.mainloop()
